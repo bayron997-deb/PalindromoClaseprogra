2fed039d6f750a05f4011cae87f6f98c7e0c445f
Bayron <bayron.m.1999@gmail.com>
2020-05-18 22:08:19 -0400

    Se crea el metodo palindromos

--- src/main/java/palindromo.java
+++ src/main/java/palindromo.java
@@ -1,3 +1,9 @@
 public class palindromo {
-    
+    public static boolean esPalindromo(String palabra){
+        //se crea un metodo que toma una palabra y la retorna
+        //instruccion: toma auxiliarpalabra, crea un builder con la variable palabra, la da vuelta y la convierte en string
+        //luego la compara y devuelve un boleean
+        String auxiliarpalabra = new StringBuilder(palabra).reverse().toString();
+        return auxiliarpalabra.equals(palabra);
+    }
 }
